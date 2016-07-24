25bb1df5cad79ccd187c7e172bafb58a62989720
Mikhail Buryak <buryak.mikhail@gmail.com>
2016-07-24 15:37:08 +0300

    migrations created

---
+++ app/database/migrations/2016_07_24_115428_create_sections_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('title', 5);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sections');
+    }
+}
